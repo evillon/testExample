5e7103be547d13d7a0b0b97de35271ce42d3a710
evillonl <evillonl@satelcorp.net>
2022-10-31 21:31:42 -0500

    agregar test de calculadora

---
+++ src/test/java/com/hiper/testexample/calculator/CalculatorServiceImplTest.java
@@ -0,0 +1,55 @@
+package com.hiper.testexample.calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@WebMvcTest(CalculatorServiceImpl.class)
+@ExtendWith(SpringExtension.class)
+class CalculatorServiceImplTest {
+
+    @Autowired
+    private CalculatorService service;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void sum() {
+        Double valor1 = 5.0;
+        Double valor2 = 5.0;
+        Double result = service.sum(valor1,valor2);
+        assertEquals(10,result);
+    }
+
+    @Test
+    void sub() {
+        Double valor1 = 5.0;
+        Double valor2 = 5.0;
+        Double result = service.sub(valor1,valor2);
+        assertEquals(0,result);
+    }
+
+    @Test
+    void multi() {
+        Double valor1 = 5.0;
+        Double valor2 = 5.0;
+        Double result = service.multi(valor1,valor2);
+        assertEquals(25,result);
+    }
+
+    @Test
+    void div() {
+        Double valor1 = 5.0;
+        Double valor2 = 5.0;
+        Double result = service.div(valor1,valor2);
+        assertEquals(1,result);
+    }
+
+}
